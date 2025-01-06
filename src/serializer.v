6d7027887542cf40420f35fd0cb1d606c4917a68
Franco_Mezzarapa <fr783884@ucf.edu>
2025-01-06 12:29:57 -0500

    :(

--- src/serializer.v
+++ src/serializer.v
@@ -1,32 +1,34 @@
-module serializer #(parameter MSG_SIZE = 64) (  // MSG_SIZE set to 8 for 8-bit data
-    input wire [MSG_SIZE - 1: 0] iData_in,      // Parallel data input (e.g., A5 in this case)
-    input wire [$clog2(MSG_SIZE):0] iCounter,   // Counter that triggers serialization
-    input wire clk,                             // Clock
-    input wire ena,                             // Enable
-    input wire rst_n,                           // Reset (active low)
+module serializer #(parameter MSG_SIZE = 64) (
+    input wire [MSG_SIZE - 1: 0] iData_in,      
+    input wire [$clog2(MSG_SIZE):0] iCounter,   
+    input wire clk,                             
+    input wire ena,                            
+    input wire rst_n,                           
     
-    output reg oData_flag,                      // Flag to indicate valid serial data
-    output reg oData_out                        // Serial data output
+    output reg oData_flag,                     
+    output reg oData_out                        
 );
 
-    integer serial_counter;                     // Counter for serialization
+    integer serial_counter;                     
     reg done_serializing;
 
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            oData_out <= 0;
-            oData_flag <= 0;
-            serial_counter <= MSG_SIZE - 1;     // Start from MSB position
-            done_serializing <= 0;
-        end 
-        else if (ena && iCounter == MSG_SIZE && !done_serializing) begin  // Check if deserialization is complete
-            oData_flag <= 1'b1;                  // Set flag during serialization
+    // Asynchronous reset
+    always @(negedge rst_n) begin
+        oData_out <= 0;
+        oData_flag <= 0;
+        serial_counter <= MSG_SIZE - 1;     
+        done_serializing <= 0;
+    end
+
+    // Synchronous logic on posedge clk
+    always @(posedge clk) begin
+        if (ena && iCounter == MSG_SIZE && !done_serializing) begin  
+            oData_flag <= 1'b1;                  
             if (serial_counter >= 0) begin
-                oData_out <= iData_in[serial_counter]; // Output the current bit (MSB first)
-                serial_counter <= serial_counter - 1;  // Decrement the counter
+                oData_out <= iData_in[serial_counter]; 
+                serial_counter <= serial_counter - 1;  
             end 
-            else if (serial_counter == -1) begin
-                // After the last bit has been output, set oData_out to 0
+            else begin
                 oData_out <= 0;                        
                 oData_flag <= 1'b0;
                 done_serializing <= 1;
