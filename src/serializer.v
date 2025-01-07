fe8f56fd31cc44f30c01f5bcc5aeb9987b4a6980
franco <vorrik200@hotmail.com>
2025-01-06 21:33:02 -0500

    Trying Stuff

--- src/serializer.v
+++ src/serializer.v
@@ -12,27 +12,29 @@ module serializer #(parameter MSG_SIZE = 64) (  // MSG_SIZE set to 8 for 8-bit d
     integer serial_counter;                     // Counter for serialization
     reg done_serializing;
 
-    always @(posedge clk or negedge rst_n) begin
+   always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             oData_out <= 0;
             oData_flag <= 0;
-            serial_counter <= MSG_SIZE - 1;     // Start from MSB position
+            serial_counter <= MSG_SIZE - 1;
             done_serializing <= 0;
         end 
-        
-        else if (ena && iCounter == MSG_SIZE && !done_serializing) begin  // Check if deserialization is complete
-            oData_flag <= 1'b1;                  // Set flag during serialization
-            if (serial_counter >= 0) begin
-                oData_out <= iData_in[serial_counter]; // Output the current bit (MSB first)
-                serial_counter <= serial_counter - 1;  // Decrement the counter
-            end 
-            else if (serial_counter == -1) begin
-                // After the last bit has been output, set oData_out to 0
-                oData_out <= 0;                        
-                oData_flag <= 1'b0;
-                done_serializing <= 1;
+        else begin
+
+            if (ena && iCounter == MSG_SIZE && !done_serializing) begin  
+                oData_flag <= 1'b1;                  
+                if (serial_counter >= 0) begin
+                    oData_out <= iData_in[serial_counter]; 
+                    serial_counter <= serial_counter - 1;  
+                end 
+
+                else if (serial_counter == -1) begin
+                    oData_out <= 0;                        
+                    oData_flag <= 1'b0;
+                    done_serializing <= 1;
+                end
             end
         end
-    end
+end
 
 endmodule
